c441fa3958ed2cdfa83c6f7639893c5ace1be2fa
Sander Demeester <demeester.sander@gmail.com>
2012-10-02 09:16:22 +0200

    Added Makefile + basic commandline parsing

---
+++ Makefile
@@ -0,0 +1,9 @@
+src=$(wildcard src/*.c)
+obj=$(addprefix obj/,$(notdir $(src:.c=.o)))
+all: AD3zip
+AD3zip: $(obj)
+	$(CC) $(CFLAGS) $(obj) -o $@
+obj/%.o: $(src)
+	$(CC) $(cflags) -c -o $@ $<
+bw : move_to_font
+move_to_front: huffman
---
+++ src/bw.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define DECODEER 0
+#define ENCODEER 1
+
+int main(int argc, char* argv[]){
+  int methode;
+  if(argc < 4){
+    fprintf(stderr, "Usage: %s [encodeer|decodeer] compressiemethode blocksize", argv[0]);
+  }
+  
+  if(!strcmp(argv[1],"encodeer")) methode = ENCODEER;
+  if(!strcmp(argv[1],"decodeer")) methode = DECODEER;
+  
+  return 0;
+}
+
