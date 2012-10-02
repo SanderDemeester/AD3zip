d6ac91f783840cc1082c5939d49c8f4863772174
Sander Demeester <demeester.sander@gmail.com>
2012-10-02 09:18:00 +0200

    added blocksize and variabels

--- src/bw.c
+++ src/bw.c
@@ -6,6 +6,8 @@
 
 int main(int argc, char* argv[]){
   int methode;
+  int compressie_methode = 0;
+  int blocksize = 0;
   if(argc < 4){
     fprintf(stderr, "Usage: %s [encodeer|decodeer] compressiemethode blocksize", argv[0]);
   }
