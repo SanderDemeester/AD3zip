9be391abf34b3f9bc5f24f0e9b5ad897d1b71d76
Sander Demeester <demeester.sander@gmail.com>
2012-10-07 21:25:54 +0200

    Toevoegen van comments

--- src/bw.c
+++ src/bw.c
@@ -52,9 +52,7 @@ static void printlist(char *rij, int *rij_index){
 }
 static int partioneer(char *rij, int *rij_index, int begin, int einde){
 
-  einde--;
-  int midden = (begin+einde)/2;
-  einde++;
+  int midden = (begin+(einde-1))/2; 
 
   #ifdef DEBUG
   printf("begin:  %d \n", begin);
@@ -62,26 +60,33 @@ static int partioneer(char *rij, int *rij_index, int begin, int einde){
   printf("einde:  %d \n", einde);
   #endif
   
+
+  //Mediaan van 3
   if(rij[rij_index[begin]] > rij[rij_index[midden]]){
+    #ifdef DEBUG
     printf("swap1 \n");
+    #endif
     swap(&rij_index[begin],&rij_index[midden]);
   }
   if(rij[rij_index[midden]] > rij[rij_index[einde]]){
+    #ifdef DEBUG
     printf("swap2 \n");
+    #endif
     swap(&rij_index[midden],&rij_index[einde]);
   }
   if(rij[rij_index[einde]] < rij[rij_index[begin]]){
+    #ifdef DEBUG
     printf("swap3 \n");
+    #endif 
     swap(&rij_index[einde],&rij_index[begin]);
   }
   
-
   
-  int spil_index = midden;
-  char spil = rij[rij_index[midden]];
-  swap(&rij_index[einde],&rij_index[spil_index]);
-  int links = begin;
-  int rechts = einde-1;
+  int spil_index = midden; //het middelste element is de index van de spil.
+  char spil = rij[rij_index[midden]]; //het spil element.
+  swap(&rij_index[einde],&rij_index[spil_index]); //wissel de index van het laatste element in de rij met de index van het spil element.
+  int links = begin; //links is gelijk aan het begin van de rij index
+  int rechts = einde-1; //begin te testen van het einde 
 
 #ifdef DEBUG
   printf("einde: %d \n", einde);
@@ -89,10 +94,12 @@ static int partioneer(char *rij, int *rij_index, int begin, int einde){
   printlist(rij,rij_index);
   printf("------------\n");
 #endif
+  //laat links wijzen naar het eerste element groter dan de spil
   while(rij[rij_index[links]] < spil){
     printf("links->element in rij: %c < spil: %c \n", rij[rij_index[links]], spil);
     links++;
   }
+  //laat rechts wijzen naar het eerste element kleiner dan de spil.
   while(rij[rij_index[rechts]] > spil){
     printf("rechts->element in rij: %c > spil: %c \n", rij[rij_index[rechts]], spil);
     rechts--;
@@ -106,7 +113,9 @@ static int partioneer(char *rij, int *rij_index, int begin, int einde){
   printf("------------\n");
 #endif
   
+  //zolang de pointers elkaar overlappen moeten we wissel acties uitvoeren.
   while(links < rechts){
+    #ifdef DEBUG
     printf("execute while links < rechts\n");
     printf("------------\n");
     printf("links: %d \n", links);
@@ -114,20 +123,23 @@ static int partioneer(char *rij, int *rij_index, int begin, int einde){
     printf("------------\n");
     printf("voor swap \n");
     printlist(rij,rij_index);
+    #endif
     swap(&rij_index[links],&rij_index[rechts]);
+    #ifdef DEBUG
     printf("na swap\n");
     printlist(rij,rij_index);
     printf("------------\n");
+    #endif
     while(rij[rij_index[links]] < spil) links++;
     while(rij[rij_index[rechts]] > spil) rechts--;
   }
   
-  spil_index = links;
+  spil_index = links; //links wijst naar de positie vlak voor het element dat groter is dan de spil.
   #ifdef DEBUG
   printf("nieuwe spil_index element is: %d \n", spil_index);
   #endif
   swap(&rij_index[spil_index],&rij_index[einde]);
-
+  
   #ifdef DEBUG
   printlist(rij,rij_index);
   #endif
