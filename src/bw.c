ef8f882cb00e05b156977ea8fe05e5dc74dead65
Sander Demeester <demeester.sander@gmail.com>
2012-10-09 14:02:37 +0200

    Verwijderen van een onnodige while lus

--- src/bw.c
+++ src/bw.c
@@ -122,13 +122,6 @@ static int partioneer(char *rij, int *rij_index, int begin, int einde){
 #ifdef DEBUG
 	printf("%c < %c\n", *(&rij[rij_index[spil_index]]+offset), *(&rij[rij_index[spil_index]]+offset));
 #endif
-	while(*(&rij[rij_index[spil_index]]+offset) == *(&rij[rij_index[rechts]]+offset)){
-	  #ifdef DEBUG
-	  printf("De volgende chars zijn nog altijd aan elkaar gelijk\n");
-	  printf("volgende char om te vergelijken in de rij is: %c\n", *(&rij[rij_index[rechts]]+offset));
-	  printf("char volgende op de spil: %c", *(&rij[rij_index[spil_index]]+offset));
-	  #endif
-	}
 	break;
       }
     }
