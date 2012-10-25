9630244121fc075d597558fbe7d4df8502b5aae7
Sander Demeester <demeester.sander@gmail.com>
2012-10-25 15:44:36 +0200

    toevoegen van comment

--- header/move_to_front.h
+++ header/move_to_front.h
@@ -1,5 +1,10 @@
 #define MTF
 
+/**************************************************************************************************/
+/* Deze struct wordt gebruikt om alle tekens van extende ascii voor te stellen.			  */
+/* Tijdens het gebruik van move to front zullen karakters regelmatig van plaats veranderen omdat  */
+/* een karakter naar de eerste pos zal worden verplaatst.					  */
+/**************************************************************************************************/
 typedef struct{
   unsigned char ascii_value;
   struct ascii_symbol *next;
