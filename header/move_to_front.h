958406f05865f1c2f533f548f4e068114ae9d16b
Sander Demeester <demeester.sander@gmail.com>
2012-10-25 15:43:03 +0200

    gebruik van dubbel gelinkte lijst van ascii tekens

--- header/move_to_front.h
+++ header/move_to_front.h
@@ -1,3 +1,10 @@
 #define MTF
 
+typedef struct{
+  unsigned char ascii_value;
+  struct ascii_symbol *next;
+  struct ascii_symbol *prev;
+} ascii_symbol;
+  
+
 void move_to_front(char *string, int len);
--- src/move_to_front.c
+++ src/move_to_front.c
@@ -5,13 +5,33 @@
 
 void move_to_front(char* string, int len){
   printf("begin move_to_front\n");
-  unsigned char index = 0;
-  unsigned char *alpha = (unsigned char*) malloc(sizeof(unsigned char)*255);
-  for(; index < (unsigned char) 255; index++){
-    alpha[index] = index;
+  ascii_symbol *first = (ascii_symbol*) malloc(sizeof(ascii_symbol));
+  ascii_symbol *t = first;
+  ascii_symbol* b;
+  first->prev = NULL;
+  first->ascii_value = '\0';
+  
+  for(int i = 0; i <= 255; i++){
+    first->next = (ascii_symbol*) malloc(sizeof(ascii_symbol));
+    b = first;
+    first = first->next;
+    first->prev = b;
+    first->ascii_value = (unsigned char)i;
   }
-  if(string[0] == alpha[49]){
-    printf("ok\n");
+  first = t;
+  for(int i = 0; i <= 255; i++){
+    first = first->next;
+    printf("%d %c \n", i, first->ascii_value);
   }
+
+
+  //start to free
+  ascii_symbol *f = first;
+  for(int i = 0; i <= 255; i++){
+    t = first->next;
+    free(f);
+    f = t;
+  }
+  
   printf("end move to front\n");
 }
