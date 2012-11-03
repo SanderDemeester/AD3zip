a09104a996e7f1cfbadf6c6ca234380489d25c0b
Sander Demeester <demeester.sander@gmail.com>
2012-11-03 16:42:16 +0100

    opbouwen van freqentie tabel

--- src/huffman.c
+++ src/huffman.c
@@ -2,6 +2,8 @@
 #include "stdio.h"
 
 void standaard_huffman(char *input_buffer, int lengte, int actie){
+  char *freq_tabel = (char*) malloc(sizeof(char)*255);
+
   if(actie){
     //encodeer
   }else{
