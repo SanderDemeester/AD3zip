f22debfc936d0b21123460ffa5d74e6cb61ec9f1
Sander Demeester <demeester.sander@gmail.com>
2012-11-06 08:46:03 +0100

    calloc huffman toppen en freeing van huffman toppen

--- src/huffman.c
+++ src/huffman.c
@@ -9,6 +9,7 @@
 
 void standaard_huffman(char *input_buffer, int lengte, int actie){
   char *freq_tabel = (char*) calloc(255,1);
+  huffman_blad **huffman_toppen = (huffman_blad**) calloc(lengte,sizeof(huffman_blad*));
 
   if(actie){
     //encodeer
@@ -16,12 +17,15 @@ void standaard_huffman(char *input_buffer, int lengte, int actie){
     for(int i = 0; i < lengte; i++) freq_tabel[(int)input_buffer[i]]++;
     for(int i = 0; i < 255; i++){
       if(freq_tabel[i] > 0){
+	huffman_toppen[i] = (huffman_blad*) calloc(1,sizeof(huffman_blad));
 	printf("symbool: %c, relatieve freq: %d \n", i, freq_tabel[i]);
       }
     }
   }else{
     //decodeer
   }
+  for(int i = 0; i < lengte; i++) free(huffman_toppen[i]);
   
   free(freq_tabel);
+  free(huffman_toppen);
 }
