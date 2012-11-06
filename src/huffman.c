7f0246b8b71b3bf37ba8333ca1161c1effb72fcd
Sander Demeester <demeester.sander@gmail.com>
2012-11-06 09:46:30 +0100

    veel te weinig gedaan

--- src/huffman.c
+++ src/huffman.c
@@ -11,14 +11,26 @@ static void swap(int *a, int *b){
   *a = *b;
   *b = t;
 }
-int static ssort(const void *x, const void *y) {
-  
+//Als je iets goed wilt (of gedaan op een manier dat je wilt snappen) doe het zelf.
+static void ssort(int *rij, int begin, int einde){
+  if(einde > begin+1){
+    int piv = rij[begin]; //piv is eerste element van rij
+    int links = begin+1;
+    int rechts = einde;
+    while(links < rechts){
+      if(rij[links] >= piv) links++;
+      else swap(&rij[links],&rij[--rechts]);
+    }
+    swap(&rij[--links],&rij[begin]);
+    ssort(rij,begin,links);
+    ssort(rij,rechts,einde);
+  }
 }
 
 void standaard_huffman(char *input_buffer, int lengte, int actie){
   int *freq_tabel = (int*) calloc(255,4);
   huffman_blad **huffman_toppen = (huffman_blad**) calloc(lengte,sizeof(huffman_blad*));
-
+  int j = 0;
   if(actie){
     //encodeer
     //Opbouwen van een freqentie tabel om de huffman boom te maken.
@@ -26,15 +38,23 @@ void standaard_huffman(char *input_buffer, int lengte, int actie){
       printf("%c \n", input_buffer[i]);
       freq_tabel[(int)input_buffer[i]]++;
     }
-    qsort(freq_tabel, 255, sizeof(int), sort);
     for(int i = 0; i < 255; i++){
-      printf("index: %i - value: %d \n", i, freq_tabel);
+      if(freq_tabel[i] > 0){
+	huffman_toppen[j] = (huffman_blad*) calloc(1,sizeof(huffman_blad));
+	huffman_toppen[j]->value = (char*) calloc(1,sizeof(char));
+	memcpy(huffman_toppen[j]->value,&i,4);
+	huffman_toppen[j]->weight = freq_tabel[i];
+      }
+    }
+    ssort(freq_tabel, 0,255);
+    for(int i = 0; i < 255; i++){
+      printf("index: %i - value: %d \n", i, freq_tabel[i]);
     }
   }else{
     //decodeer
   }
   for(int i = 0; i < lengte; i++){
-    //    free(huffman_toppen[i]->value);
+    free(huffman_toppen[i]->value);
     free(huffman_toppen[i]);
   }
   
