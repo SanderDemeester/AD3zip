1ca0a6ebc10b0ce1261d5eec803aa3e7ab63aad4
Sander Demeester <demeester.sander@gmail.com>
2012-11-27 22:25:08 +0100

    Toevoegen van nieuwe test plot scripts

--- tests/plot_scripts/script.sh
+++
@@ -1,6 +0,0 @@
-for i in $(seq 5); do
-    sed -n "/blocksize: $i/{N;p}" r >> blocksize_$i
-    sed -i 's/^real *\t *0m//g' blocksize_$i
-    sed -i 's/^filesize: \([0-9]\{,2\}\).*$/\1/g' blocksize_$i
-    sed -i '/^[0-9]\+$/{:a;N;$!ba;s/\n/ /g};s/s /\n/g' blocksize_$i
-done
